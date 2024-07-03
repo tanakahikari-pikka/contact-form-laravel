d43c0badbfc7b8892f561f815b9bf18048c38590
tanakahikari-pikka <hikaricareer1999@gmail.com>
2024-07-03 21:53:43 +0900

    マイグレーション

---
+++ src/database/migrations/2024_07_03_123825_create_contacts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('categry_id');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->tinyInteger('gender');
+            $table->string('email', 255);
+            $table->string('tell', 255);
+            $table->string('address', 255);
+            $table->string('building', 255)->nullable();
+            $table->text('detail')->nullable();
+            $table->timestamps();
+
+            $table->foreign('categry_id')->references('id')->on('categories');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
