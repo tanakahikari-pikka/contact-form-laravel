4eb5a84ab5dfd2ab272b2e577e78cfbf270cea62
tanakahikari-pikka <hikaricareer1999@gmail.com>
2024-07-03 22:14:37 +0900

    seed: カテゴリー

---
+++ src/database/factories/CategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'content' => $this->faker->word,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AuthorsTableSeeder::class);
+        $this->call([
+            CategoriesTableSeeder::class,
+        ]);        
         // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
