664ec040505adcc2f65c8059d95b00eb523f9de7
tanakahikari-pikka <hikaricareer1999@gmail.com>
2024-07-03 22:38:14 +0900

    seed: contact

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            ContactsTableSeeder::class,
             CategoriesTableSeeder::class,
         ]);        
         // \App\Models\User::factory(10)->create();
