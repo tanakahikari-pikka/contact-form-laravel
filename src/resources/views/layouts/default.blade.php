b5763f3f0ff86e3530a9cc12bd0162018c025825
tanakahikari-pikka <hikaricareer1999@gmail.com>
2024-07-05 02:09:47 +0900

    ヘッダーのデザイン調整

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -4,22 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <!-- Materialize -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <link rel="stylesheet" href="{{ asset('css/reset.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
-    <header>
-        <nav>
-            <div class="nav-wrapper amber lighten-5 row">
-                <a href="#" class="brand-logo center black-text">FashionablyLate</a>
-                <a href="#"  class="right col s2 black-text">login</a>
-            </div>
-        </nav>
-    </header>
-    <div class="content">
-        <h2 class="blown-text text-lighten-1 center heading h4">@yield('title')</p>
-        @yield('content')
+    <div class="container">
+        <header class="header">
+            <h1 class="header_logo">FashionablyLate</h1>
+            <a class="header_button" href="">button</a>
+            <!-- @yield('enter') -->
+        </header>
+        <div class="content">
+            @yield('title')
+            @yield('content')
+        </div>
     </div>
 </body>
 </html>
