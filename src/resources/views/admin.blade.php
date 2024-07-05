203c2c4ee78c55bc17db07714ab211450c6a1668
tanakahikari-pikka <hikaricareer1999@gmail.com>
2024-07-06 02:11:52 +0900

    adminフロント実装

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -1,14 +1,82 @@
 @extends('layouts.default')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/admin.css')}}">
+@endsection
+
 @section('title', 'admin')
 
 @section('content')
 @section('heading', 'Admin')
 <div class="container">
-    <div class="row">
-        <div class="col s12">
-            <p>管理画面</p>
+    <!-- テキストフォーム -->
+    <form class="form-inline" action="" method="post">
+        @csrf
+        <div class="d-flex justify-content-between mt-2 mb-2">
+            <input type="text" name="name" class="mail-form" id="name" placeholder="名前やメールアドレスを入力してください">
+            <!-- 選択時のデザインがダサいのでカスタムドロップダウンを使うかも -->
+            <select name="select" id="select" class="select-form">
+                <option value="" selected disabled>性別</option>
+                <option value="1">男性</option>
+                <option value="2">女性</option>
+                <option value="3">その他</option>
+            </select>
+            <select name="select" id="select" class="select-form">
+                <option value="" selected disabled>お問い合わせの種類</option>
+                <option value="1">カテゴリ1</option>
+                <option value="2">カテゴリ2</option>
+                <option value="3">カテゴリ3</option>
+            </select>
+            <!-- 日付選択 -->
+            <input type="date" name="date" id="date" class="date-form">
+            <button type="submit" class="btn-serch bg-dark-brawn white">検索</button>
+            <button type="submit" class="btn-reset bg-lighter-brawn white">リセット</button>
         </div>
-    </div>
+        <div class="d-flex justify-content-between mb-1">
+            <button type="submit" class="btn-export bg-ash">エクスポート</button>
+            <!-- ページネーション -->
+            <div class="pagination">
+                <ul class="pagination
+                justify-content-center">
+                    <li class="page-item disabled">
+                        <a class="page-link" href="#" tabindex="-1">前へ</a>
+                    </li>
+                    <li class="page-item active">
+                        <a class="page-link" href="#">1</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">2</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">3</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">次へ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <table class="table">
+            <thead>
+                <tr class="bg-dark-brawn white text-align-left">
+                    <th>お名前</th>
+                    <th>性別</th>
+                    <th>メールアドレス</th>
+                    <th>お問い合わせの種類</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="border-bottom">
+                    <td class="border-left">山田太郎</td>
+                    <td>男性</td>
+                    <td>mainl@main.com</td>
+                    <td>カテゴリ1</td>
+                    <td class="border-right">
+                        <button type="submit" class="btn-show">詳細</button>
+                    </td>
+                </tr>
+            </tbody>
+    </form>
 </div>
 @endsection
