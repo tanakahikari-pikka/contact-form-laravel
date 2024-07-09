cade98656c7c662c9add43ba299451394e30df61
tanakahikari-pikka <hikaricareer1999@gmail.com>
2024-07-10 00:03:06 +0900

    yaml修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -17,11 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategoriesTableSeeder::class,
             ContactsTableSeeder::class,
-        ]);        
-        // \App\Models\User::factory(10)->create();
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ]);
     }
 }
